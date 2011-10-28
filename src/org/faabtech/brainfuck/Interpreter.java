d9532b997e4b94f1cadf2a8fbf1c3f5942ba02b4
Fabian M. <mail.fabianm@gmail.com>
2011-10-28 18:02:13 +0300

    Added new imports. Forgot them :->

--- src/org/faabtech/brainfuck/Interpreter.java
+++ src/org/faabtech/brainfuck/Interpreter.java
@@ -3,7 +3,10 @@ package org.faabtech.brainfuck;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 /**
  * The {@link Interpreter} class interpret the given <code>BrainFuck</code> code.
